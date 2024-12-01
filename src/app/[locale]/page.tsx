2d39a4832b14f8249511e6a47a9db432db94998f
Zammouri_Youssef <zammouri.yousseff@gmail.com>
2024-12-01 19:10:14 +0100

    Commiting changes from dev

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,20 +3,16 @@ import {Link} from '@/i18n/routing';
 import {Button} from "@/components/ui/button";
 import {LanguageSwitcher} from "@/components/ui/LanguageSwitcher";
 import ThemeSwitcher from "@/components/ui/ThemeSwitcher";
-import {auth} from "@/lib/auth";
-import {getTranslations} from "next-intl/server";
 
-const HomePage = async({ params: {locale}}: { params: {locale: string}; }) => {
-  const t = await getTranslations('HomePage');
-    const session = await auth()
-    console.log(session)
+const HomePage = ({ params: {locale}}: { params: {locale: string}; }) => {
+  const t = useTranslations('HomePage');
   return (
       <div>
         <h1>{t('title')}</h1>
           <LanguageSwitcher locale={locale} />
           <ThemeSwitcher size={"md"}/>
           <Button><Link href={"/about"}>{t('about')}</Link></Button>
-          {JSON.stringify(session)}
+
       </div>
 
   );
