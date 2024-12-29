9d77d1d6a777fa10327edd9c20873c38279240c5
Zammouri_Youssef <zammouri.yousseff@gmail.com>
2024-12-29 18:18:54 +0100

    adding responsiveness to homepage

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,11 +1,17 @@
 import {ReactNode} from "react";
 import {Toaster} from "@/components/ui/toaster";
+import {getMessages} from "next-intl/server";
+import { NextIntlClientProvider } from "next-intl";
+
 
 export default async function RootLayout({children, params: {locale}}: { children: ReactNode; params: {locale: string}; }) {
+  const messages = await getMessages();
 
   return (
     <div style={{direction: locale === "ar" ? "rtl" : "ltr"}}>
-    {children}
+    <NextIntlClientProvider messages={messages}>
+        {children}
+      </NextIntlClientProvider>
     <Toaster />
     </div>
   );
--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,39 +1,64 @@
 'use client'
 import { useTranslations } from 'next-intl';
-import React from 'react'
+import React from 'react';
 
-export default function Modal({ isVisible, onClose }: { isVisible: boolean; onClose: () => void } ) {
+export default function Modal({
+  isVisible,
+  onClose,
+}: {
+  isVisible: boolean;
+  onClose: () => void;
+}) {
   const t = useTranslations('HomePage');
 
-    if( !isVisible ) return null;
-    
-    const handleClose = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        // if (e.target.id === 'wrapper') onClose();
-        if ((e.target as HTMLDivElement).id === 'wrapper') {
-            onClose();
-          }
-      };
+  if (!isVisible) return null;
+
+  const handleClose = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if ((e.target as HTMLDivElement).id === 'wrapper') {
+      onClose();
+    }
+  };
 
   return (
     <>
-      <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex min-h-full flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8 z-50"
-        id="wrapper" onClick={handleClose}>
-        <div className='w-[1100px] flex flex-col'>
-            <button className="text-white text-xl place-self-end" 
-            onClick={() => onClose()}>x</button>
-            <div className='bg-white rounded flex justify-between'>
-            {/* <div className="mx-auto h-10 w-auto"> */}
-            <div className='bg-black w-[700px] flex flex-col justify-center pl-2'>
-              <h1 className='text-white text-5xl font-bold pb-4'>{t('navigation.nav1')}</h1>
-              <h3 className='text-myColor2 text-3xl font-bold pb-4'>{t('aboutus.subtitle')}</h3>
-              <p className='text-white text-lg font-bold	'>{t('aboutus.paragraphe')}</p>
+      <div
+        className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center z-50 px-4 sm:px-8"
+        id="wrapper"
+        onClick={handleClose}
+      >
+        <div className="w-full max-w-[1100px] flex flex-col bg-black rounded-lg overflow-hidden">
+          {/* Close Button */}
+          <button
+            className="self-end p-2 text-white bg-black rounded hover:bg-gray-800 transition"
+            onClick={() => onClose()}
+          >
+            X
+          </button>
+          {/* Modal Content */}
+          <div className="flex flex-col md:flex-row items-center">
+            {/* Left Section */}
+            <div className="bg-black p-6 w-full md:w-2/3 flex flex-col justify-center text-center md:text-left">
+              <h1 className="text-white text-3xl sm:text-4xl font-bold mb-4">
+                {t('navigation.nav1')}
+              </h1>
+              <h3 className="text-myColor6 text-2xl sm:text-3xl font-bold mb-4">
+                {t('aboutus.subtitle')}
+              </h3>
+              <p className="text-white text-base sm:text-lg font-semibold">
+                {t('aboutus.paragraphe')}
+              </p>
             </div>
-            <div>
-              <img src="/assets/images/aboutUsimg.png" alt="aboutUsimg" />
+            {/* Right Section: Image */}
+            <div className="hidden md:block md:w-1/3">
+              <img
+                src="/assets/images/aboutUsimg.png"
+                alt="aboutUsimg"
+                className="w-full h-[500px] max-w-sm mx-auto"
+              />
             </div>
-            </div>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
